875d664541a27bc3cad2fd8fa6bc5a132e9d6055
leonardobussi <leonardobezerrabussy@gmail.com>
2021-06-12 14:30:36 -0300

    boa

--- app/controllers/investiment_controller.rb
+++ app/controllers/investiment_controller.rb
@@ -1,53 +1,59 @@
 class InvestimentController < ApplicationController
     
-    def initialize
-        @summary_service = GlobalSummary.new
-				@message = ''
-    end
+	def initialize
+			@summary_service = GlobalSummary.new
+			@message = ''
+	end
 
-    def home
-        @hello = "Bem-vindo a pagina principal"
-    end
-    
-    def index
-        @summarys = @summary_service.index
-    end
+	def home
+			@hello = "Bem-vindo a pagina principal"
+	end
+	
+	def index
+			@summarys = @summary_service.index
+	end
 
-    def show
-        @summary = @summary_service.show(params[:invest_id]) 
-    end
+	def show
+			@summary = @summary_service.show(params[:invest_id]) 
+	end
 
-    def create
-				get_params(params['post'])
-    end
+	def new;end
 
-    private
+	def create
+		get_params(params['post'])
+	end
 
-    def get_params(params)
+	private
 
-			result = {
-				"investment_name": params['nome'],
-				"status": params['status'],
-				"risk": params['risco'],
-				"category": params['categoria'],
-				"consortium": "60b03825fcf1440015381f38",
-				"profitability": {
-					"day": params['dia'],
-					"month": params['mes'],
-					"year": params['ano'],
-					"score": params['score'],
-					"minimum_value": params['minimo']
-				}
+	def get_params(params)
+
+		result = json_parser(params)
+
+		@summary_new = @summary_service.create(result)
+	
+		if @summary_new['status'] == 201
+			@message = 'deu bom meu rapaz'
+		else
+			@message = 'deu rium meu rapaz'
+		end
+
+		puts @summary_new
+	end
+
+	def json_parser(params)
+		{
+			"investment_name": params['nome'],
+			"status": params['status'],
+			"risk": params['risco'],
+			"category": params['categoria'],
+			"consortium": "60b03825fcf1440015381f38",
+			"profitability": {
+				"day": params['dia'],
+				"month": params['mes'],
+				"year": params['ano'],
+				"score": params['score'],
+				"minimum_value": params['minimo']
 			}
-
-			@summary_new = @summary_service.create(result)
-		
-			if @summary_new['status'] == 201
-				@message = 'deu bom meu rapaz'
-			else
-				@message = 'deu rium meu rapaz'
-			end
-    end
-
-		def new;end
+		}
+	end
 end
