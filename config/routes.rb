f18cde598cb758bea8abfa7e099166b059fee95a
leonardobussi <leonardobezerrabussy@gmail.com>
2021-06-24 17:21:38 -0300

    adding posible custumer controller of users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users # , except: %i[destroy]
+  # get '/users/sign_out' => 'devise/sessions#destroy'
+
   root to: 'home#home'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   get '/investment/specific', action: 'show', controller: 'investiment'
   get '/investment/edition', action: 'edit', controller: 'investiment'
   post '/investment/updated/', action: 'update', controller: 'investiment'
