ff27b3962069d0add8f0b4dffb7e69459f2fae5f
leonardobussi <leonardobezerrabussy@gmail.com>
2021-06-23 22:24:05 -0300

    inicializado a autenticação de usuario

--- app/controllers/consortium_controller.rb
+++ app/controllers/consortium_controller.rb
@@ -1,61 +1,58 @@
 class ConsortiumController < ApplicationController
-  
+  before_action :authenticate_user!
+
   def initialize
     @summary_service = GlobalConsortium.new
     @message = ''
   end
 
-  
   def index
     @summarys = @summary_service.index
   end
 
-  def new;end
+  def new; end
 
   def create
     get_params_create(params['post'])
   end
 
   def edit
-		@summary_show = @summary_service.show(params['conso_id'])
-	end
+    @summary_show = @summary_service.show(params['conso_id'])
+  end
 
   def update
-		get_params_update(params['post']['conso_id'], params['post'])
+    get_params_update(params['post']['conso_id'], params['post'])
   end
 
-  def destroy
-  end
+  def destroy; end
 
   private
 
   def get_params_create(params)
+    result = json_parser(params)
 
-		result = json_parser(params)
+    @summary_new = @summary_service.create(result)
 
-		@summary_new = @summary_service.create(result)
-	
-		if @summary_new['status'] == 201
-			@message = 'deu bom meu rapaz'
-		else
-			@message = 'deu rium meu rapaz'
-		end
-	end
+    @message = if @summary_new['status'] == 201
+                 'deu bom meu rapaz'
+               else
+                 'deu rium meu rapaz'
+               end
+  end
 
+  def get_params_update(id, params)
+    result = json_parser(params)
+    @summary_update = @summary_service.update(id, result)
+    @message = if @summary_update['status'] == 200
+                 'deu bom meu rapaz'
+               else
+                 'deu ruim meu rapaz'
+               end
+  end
 
-	def get_params_update(id, params)
-		result = json_parser(params)
-		@summary_update = @summary_service.update(id, result)
-		if @summary_update['status'] == 200 
-			@message = 'deu bom meu rapaz'
-		else
-			@message = 'deu ruim meu rapaz'
-		end
-	end
-
-	def json_parser(params)
-		{
-			"consortium_name": params['nome']
-		}
-	end
+  def json_parser(params)
+    {
+      "consortium_name": params['nome']
+    }
+  end
 end
--- app/controllers/investiment_controller.rb
+++ app/controllers/investiment_controller.rb
@@ -1,82 +1,80 @@
 class InvestimentController < ApplicationController
-    
-	def initialize
-			@summary_service = GlobalInvestment.new
-			@message = ''
-	end
+  before_action :authenticate_user!
+  def initialize
+    @summary_service = GlobalInvestment.new
+    @message = ''
+  end
 
-	def index
-			@summarys = @summary_service.index
-	end
+  def index
+    @summarys = @summary_service.index
+  end
 
-	def show
-			@summary = @summary_service.show(params[:invest_id]) 
-	end
+  def show
+    @summary = @summary_service.show(params[:invest_id])
+  end
 
-	def new;end
+  def new; end
 
-	def create
-		get_params_create(params['post'])
-	end
+  def create
+    get_params_create(params['post'])
+  end
 
-	def edit
-		@summary_edit = @summary_service.show(params[:invest_id])
-	end
+  def edit
+    @summary_edit = @summary_service.show(params[:invest_id])
+  end
 
-	def update
+  def update
     get_params_update(params['post']['invest_id'], params['post'])
   end
 
-	def destroy
-		@summary_delete = @summary_service.delete(params['invest_id'])
+  def destroy
+    @summary_delete = @summary_service.delete(params['invest_id'])
 
-		if @summary_delete['status'] == 404
-			@message = 'usuario já foi excluido'
-		else
-			@message = 'sucesso ao excluir'
-		end
-	end
+    @message = if @summary_delete['status'] == 404
+                 'usuario já foi excluido'
+               else
+                 'sucesso ao excluir'
+               end
+  end
 
-	private
+  private
 
-	def get_params_create(params)
+  def get_params_create(params)
+    result = json_parser(params)
 
-		result = json_parser(params)
+    @summary_new = @summary_service.create(result)
 
-		@summary_new = @summary_service.create(result)
-	
-		if @summary_new['status'] == 201
-			@message = 'deu bom meu rapaz'
-		else
-			@message = 'deu rium meu rapaz'
-		end
-	end
+    @message = if @summary_new['status'] == 201
+                 'deu bom meu rapaz'
+               else
+                 'deu rium meu rapaz'
+               end
+  end
 
+  def get_params_update(id, params)
+    result = json_parser(params)
+    @summary_update = @summary_service.update(id, result)
+    @message = if @summary_update['status'] == 200
+                 'deu bom meu rapaz'
+               else
+                 'deu rium meu rapaz'
+               end
+  end
 
-	def get_params_update(id, params)
-		result = json_parser(params)
-		@summary_update = @summary_service.update(id, result)
-		if @summary_update['status'] == 200 
-			@message = 'deu bom meu rapaz'
-		else
-			@message = 'deu rium meu rapaz'
-		end
-	end
-
-	def json_parser(params)
-		{
-			"investment_name": params['nome'],
-			"status": params['status'],
-			"risk": params['risco'],
-			"category": params['categoria'],
-			"consortium": params['consorcio_id']  || "60b03825fcf1440015381f38",
-			"profitability": {
-				"day": params['dia'],
-				"month": params['mes'],
-				"year": params['ano'],
-				"score": params['score'],
-				"minimum_value": params['minimo']
-			}
-		}
-	end
+  def json_parser(params)
+    {
+      "investment_name": params['nome'],
+      "status": params['status'],
+      "risk": params['risco'],
+      "category": params['categoria'],
+      "consortium": params['consorcio_id'] || '60b03825fcf1440015381f38',
+      "profitability": {
+        "day": params['dia'],
+        "month": params['mes'],
+        "year": params['ano'],
+        "score": params['score'],
+        "minimum_value": params['minimo']
+      }
+    }
+  end
 end
